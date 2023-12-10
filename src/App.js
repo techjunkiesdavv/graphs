656f065c2450e1e24c060e83e2609377e0de7aa7
Harsh Farkiya <108735661+HarshhFarkiya@users.noreply.github.com>
2023-12-10 15:38:46 +0530

    graphs linked added

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <a href="https://657586035345c1064c843b48--jovial-stardust-dfd288.netlify.app/" target="_blank">First</a>
+     <a href="https://65758494648ed707d91771ef--aesthetic-bublanina-6b3be8.netlify.app" target="_blank">Second</a>
+     <a href="https://65758214fc899003d5c1281c--incomparable-yeot-a1b61b.netlify.app" target="_blank">Third</a>
+     <a href="https://6575897ac999e5075b826a33--tourmaline-manatee-891920.netlify.app/" target="_blank">Fourth</a>
+     <a href="https://horizon-ui.com/horizon-ui-chakra/#/admin/default" target="_blank">Fifth</a>
+     <a href="https://berrydashboard.io/" target="_blank">Sixth</a>
+     <a href="https://6575897ac999e5075b826a33--tourmaline-manatee-891920.netlify.app/" target="_blank">Seventh</a>
     </div>
   );
 }
